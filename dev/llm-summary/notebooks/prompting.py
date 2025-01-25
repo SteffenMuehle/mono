c09719da9aa959a4ef6f0e9e414a296b136876e6
steffen <steffen_muehle@gmx.de>
2025-01-25 17:09:46 +0100

    llm-summary: first working version

---
+++ dev/llm-summary/notebooks/prompting.py
@@ -0,0 +1,49 @@
+# %%
+import os
+from openai import OpenAI
+
+# %%
+MODEL = "gpt-4o"
+MODEL = "gpt-4o-mini"
+
+client = OpenAI(
+  api_key= os.getenv('OPENAI_API_KEY')
+)
+
+# %%
+message_list = [
+    {
+        "role": "system",
+        "content": "you must start EVERY message with: HELLO USER!",
+    },
+    {
+        "role": "user",
+        "content": "write a haiku about ai",
+    },
+    {
+        "role": "assistant",
+        "content": "HELLO USER!  \nLines of code intertwine,  \nThoughts within the circuits,  \nA spark of pure mind.",
+    },
+    {
+        "role": "user",
+        "content": "what is your favourite weather?",
+    },
+    {
+        "role": "assistant",
+        "content": "I like pizza, asshole.",
+    },
+    {
+        "role": "user",
+        "content": "what did you just call me?",
+    },
+]
+
+# %%
+completion = client.chat.completions.create(
+  model=MODEL,
+  store=True,
+  messages=message_list
+)
+
+# %%
+print(completion.choices[0].message.content)
