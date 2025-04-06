c0f8e3cb4136c4a7461c8910042caf2b7c62a9cc
steffen <steffen_muehle@gmx.de>
2025-04-06 17:23:46 +0200

    support different banks when parsing depot csv files

--- finances/src/finances/parse_depot.py
+++ finances/src/finances/parse_depot.py
@@ -2,20 +2,27 @@ import csv
 import toml
 from pathlib import Path
 
-def update_toml_from_csv(toml_file_path_in, toml_file_path_out, csv_file_path):
+def update_dict_from_csv(toml_dict, csv_file_path, bank):
+    id_column = {
+        "comdirect": "WKN",
+        "ing": "ISIN",
+    }[bank]
+
+    value_column = {
+        "comdirect": "Wert in EUR",
+        "ing": "Kurswert",
+    }[bank]
+
     # Read the CSV file and store the data in a dictionary
     csv_data = {}
     csv_ids = set()
     with open(csv_file_path, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter=';')
         for row in csv_reader:
-            wkn = row['WKN']
-            value = float(row['Wert in EUR'].replace('.', '').replace(',', '.'))
+            wkn = row[id_column]
+            value = float(row[value_column].replace('.', '').replace(',', '.'))
             csv_data[wkn] = value
             csv_ids.add(wkn)
-
-    # Read the TOML file
-    toml_dict = toml.load(toml_file_path_in)
 
     # Lists to keep track of altered and not found entries
     altered_entries = []
@@ -50,9 +57,7 @@ def update_toml_from_csv(toml_file_path_in, toml_file_path_out, csv_file_path):
     # Update the TOML data
     update_toml(toml_dict)
 
-    # Write the updated TOML data back to the file
-    with open(toml_file_path_out, 'w') as toml_file:
-        toml.dump(toml_dict, toml_file)
+    print("\n###############################################")
 
     # Print the results
     print("Altered entries:")
@@ -67,19 +72,35 @@ def update_toml_from_csv(toml_file_path_in, toml_file_path_out, csv_file_path):
     for csv_id in csv_ids:
         print(f" - {csv_id}")
 
+    print("\n###############################################\n")
+    
+    return toml_dict
+
 if __name__ == "__main__":
-    relative_toml_folder_path = Path(__file__).parent.parent.parent / "data" / "input" / "graph"
-    relative_csv_folder_path = Path(__file__).parent.parent.parent / "data" / "input" / "depot"
-    most_recent_csv_file = sorted(relative_csv_folder_path.glob("*.csv"))[-1]
-    csv_file_path = relative_csv_folder_path / most_recent_csv_file
-
-    print(f"Using most recent CSV file: {csv_file_path}")
-
+    toml_folder_path = Path(__file__).parent.parent.parent / "data" / "input" / "graph"
     for toml_file_name in [
         'investments_manifest.toml',
         'elisa_manifest.toml',
     ]:
-        print("\n###############################################")
-        print(f"Updating {toml_file_name} from {csv_file_path}")
-        update_toml_from_csv(relative_toml_folder_path / toml_file_name, relative_toml_folder_path / toml_file_name.replace("_manifest",""), csv_file_path)
-        print("###############################################\n")
+        # Read the TOML file
+        print(f"Updating {toml_file_name}...")
+        toml_dict = toml.load(toml_folder_path / toml_file_name)
+
+        for bank in ["comdirect", "ing"]:
+            csv_folder = Path(__file__).parent.parent.parent / "data" / "input" / "depot" / bank
+            most_recent_csv_file = sorted(csv_folder.glob("*.csv"))[-1]
+            csv_file_path = csv_folder / most_recent_csv_file
+
+            print(f"Updating with csv from '{bank}': {csv_file_path}")
+
+            toml_dict = update_dict_from_csv(
+                toml_dict=toml_dict,
+                csv_file_path=csv_file_path,
+                bank=bank,
+            )
+            
+
+        # Write the updated TOML data back to the file
+        toml_file_path_out=toml_folder_path / toml_file_name.replace("_manifest","")
+        with open(toml_file_path_out, 'w') as toml_file:
+            toml.dump(toml_dict, toml_file)
